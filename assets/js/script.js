04e120c9660494fb9cfbfb6cba484f22e3d16459
vivienne <vivienne.2214@yahoo.com>
2022-08-06 11:15:03 +0000

    add run game function and displayChoices function with images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// global variables for available choices
+let choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
 // wair for the DOM to load content before running game
 // get buttons and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
@@ -12,3 +15,43 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 } );
+
+/** 
+ * this is the main function which runs the game
+ * @param {string} playerChoice: the choice made by the Player
+ */
+ function game(playerChoice) {
+
+    //  creates random choice for the computer
+    let randomNumber = Math.floor(Math.random() * 5); 
+    let computerChoice = choices[randomNumber];
+
+    displayChoices(playerChoice, computerChoice);
+}
+
+/**
+ * this function displays the choice made by the player and computer in the div
+ * pairs up the choice buttons with the respective images
+ * @param {string} playerChoice: the choice made by the Player
+ * @param {string} computerChoice: the random choice made by Computer
+ */
+function displayChoices(playerChoice, computerChoice) {
+    
+    let playerZone = document.getElementById('playerZone');
+    let computerZone = document.getElementById('computerZone');
+
+    let playerImage = 'assets/images/' + playerChoice + '.png';
+    let computerImage = 'assets/images/' + computerChoice + '.png';
+
+    let displayChoice1 = document.createElement('img');
+    let displayChoice2 = document.createElement('img');
+
+    displayChoice1.setAttribute('class', 'images');
+    displayChoice1.src = playerImage;
+
+    displayChoice2.setAttribute('class', 'images');
+    displayChoice2.src = computerImage;
+
+    playerZone.appendChild(displayChoice1);
+    computerZone.appendChild(displayChoice2);
+}
